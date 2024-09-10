3baad43a86f916485d8c0dc6e0a7ac43744c967f
JonathanGhebrial <jony@ghebrial.com>
2024-09-10 12:21:38 -0700

    fix cv page

--- client/src/components/CVPage.js
+++ client/src/components/CVPage.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Viewer, Worker } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import resumePDF from '../assets/Jonathan Ghebrial - Resume.pdf';
+import { version as pdfjsVersion } from 'pdfjs-dist';
 
 const CVPage = () => {
     return (
         <div className="cv-page">
-            <Worker workerUrl={`https://unpkg.com/pdfjs-dist@2.6.347/build/pdf.worker.min.js`}>
+            <Worker workerUrl={`https://unpkg.com/pdfjs-dist@${pdfjsVersion}/build/pdf.worker.min.js`}>
                 <div style={{ height: '750px' }}>
                     <Viewer fileUrl={resumePDF} />
                 </div>
